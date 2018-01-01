0f9d0e4e3f648b36424844c70e4df85ed7c6e82d
zeeshan634 <zeeshan5x@yahoo.com>
2018-01-01 15:45:58 +0500

    config file updated

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/verifan/';
+$config['base_url'] = 'http://localhost/vrfn/';
 
 $config['admin_email'] = 'alexrobbio860@gmail.com';
 
